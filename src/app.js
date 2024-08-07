c296ec6cd3a903ec404311db9c1c698225f4c1f2
sneha <snehandckap@gmail.com>
2024-08-07 17:00:51 +0530

    APi update

--- src/app.js
+++ src/app.js
@@ -39,8 +39,9 @@ import eventRoute from '../src/routes/event.routes.js';
 import packageRouter from "./routes/package.routes.js";
 
 app.use(`${apiBasePath}/events`, eventRoute);
-app.use('/api', eventRoute);
 app.use("/api/allpackages", packageRouter);
+app.use('/api', eventRoute);
+
 
 // Default home page route
 app.use("/", (req, res) => {
